618db8ec0c7d3da1c7f7353526e3bc8da7d1c48c
Kanani <kaneusb@yahoo.fr>
2026-06-07 15:51:30 +0200

    remove comment

--- components/phone-auth.js
+++ components/phone-auth.js
@@ -25,16 +25,6 @@ const FIREBASE_CONFIG = {
     appId:             "%%FIREBASE_APP_ID%%",
 };
 
-/*
-const FIREBASE_CONFIG = {
-    apiKey:            "REPLACE_WITH_YOUR_API_KEY",
-    authDomain:        "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-    projectId:         "REPLACE_WITH_YOUR_PROJECT_ID",
-    storageBucket:     "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-    messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-    appId:             "REPLACE_WITH_YOUR_APP_ID",
-};
-*/
 /* ══════════════════════════════════════════════════════════════ */
 
 const LABELS = {
